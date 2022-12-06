d82995ff406dc956390b88e97adc7d0553b23dde
LeDrew2017 <ledrew101806@gmail.com>
2022-12-05 20:00:45 -0500

    added banana-13

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/xtended_beyond0lte.mk
+    $(LOCAL_DIR)/banana_beyond0lte.mk
 
 COMMON_LUNCH_CHOICES := \
-    xtended_beyond0lte-eng \
-    xtended_beyond0lte-user \
-    xtended_beyond0lte-userdebug
+    banana_beyond0lte-eng \
+    banana_beyond0lte-user \
+    banana_beyond0lte-userdebug
